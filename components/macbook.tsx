daa97723dbfa4e0d78fe7e33db164f2c75109a31
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-07-03 18:58:50 +0530

    ui : macbook component 🚀

--- components/macbook.tsx
+++ components/macbook.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import styles from "./macbook.module.css"; // Import as an object
 
 const Macbook = () => {
   return (
-    <div className=" min-h-screen flex justify-center  p-2 sm:p-10 ">
-      <div className="max-w-5xl w-full px-4    flex flex-col relative ">
-        <div className=" flex-grow h-full w-full">
+    <div className="min-h-screen flex justify-center p-2 sm:p-10">
+      <div className="max-w-5xl w-full px-4 flex flex-col relative">
+        <div className="flex-grow h-full w-full">
           <Image
             src="/macbook.webp"
             width={1000}
@@ -14,7 +15,9 @@ const Macbook = () => {
             unoptimized
           />
         </div>
-        <h1 className="text-2xl md:text-5xl font-bold text-center px-2 mt-5 bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent p-2">
+        <h1
+          className={`${styles["gradientText"]} text-2xl md:text-5xl font-bold text-center px-2 mt-5 first-line:gradient-text p-2 `}
+        >
           &quot;Transforming Visions into Innovative, High-Quality
           Applications&quot;
         </h1>
